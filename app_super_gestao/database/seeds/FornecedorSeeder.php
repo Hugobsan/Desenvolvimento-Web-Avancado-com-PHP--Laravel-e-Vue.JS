d656b4da521ba76d6226243a5633bff120c5defc
Hugobsan <hugocorone10@gmail.com>
2024-01-22 13:08:18 -0300

    Add fillable fields to SiteContato model and update DatabaseSeeder

---
+++ app_super_gestao/database/seeds/FornecedorSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Fornecedor;
+use Illuminate\Support\Facades\DB;
+
+class FornecedorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $fornecedor = new Fornecedor();
+        $fornecedor->nome = 'Fornecedor 100';
+        $fornecedor->site = 'fornecedor100.com.br';
+        $fornecedor->uf = 'SP';
+        $fornecedor->email = 'contato@fornecedor100.com.br';
+        $fornecedor->save();
+
+        // Utilizando o método create (atenção para o atributo fillable da classe)
+        Fornecedor::create([
+            'nome' => 'Fornecedor 200',
+            'site' => 'fornecedor200.com.br',
+            'uf' => 'SP',
+            'email' => 'contato@fornecedor200.com.br'
+        ]);
+
+        // Insert
+        DB::table('fornecedores')->insert([
+            'nome' => 'Fornecedor 300',
+            'site' => 'fornecedor300.com.br',
+            'uf' => 'SP',
+            'email' => 'contato@fornecedor300.com.br'
+        ]);
+
+    }
+}
---
+++ app_super_gestao/database/seeds/SiteContatoSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\SiteContato;
+
+class SiteContatoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        SiteContato::create([
+            'nome' => 'Sistema SG',
+            'telefone' => '(11) 99999-9999',
+            'email' => 'contato@sg.com.br ',
+            'motivo_contato' => 1,
+            'mensagem' => 'Seja bem-vindo ao sistema Super Gestão',
+        ]);
+    }
+}
